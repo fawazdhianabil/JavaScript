8cbced4c834335e91cc74f18c46b5524f0f531c1
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2022-12-27 19:45:02 +0700

    Add files via upload

--- tipe_array.js
+++ tipe_array.js
@@ -109,3 +109,20 @@ names.forEach((name) => {
 });
 
 // note pada array for each, array map dan array filter tidak bisa menggunakan break, continue
+
+// tes coding
+const books = [
+    { title: 'The Da Vinci Code', author: 'Dan Brown', sales: 5094805 },
+    { title: 'The Ghost', author: 'Robert Harris', sales: 807311 },
+    { title: 'White Teeth', author: 'Zadie Smith', sales: 815586 },
+    { title: 'Fifty Shades of Grey', author: 'E. L. James', sales: 3758936 },
+    { title: 'Jamie\'s Italy', author: 'Jamie Oliver', sales: 906968 },
+    { title: 'I Can Make You Thin', author: 'Paul McKenna', sales: 905086 },
+    { title: 'Harry Potter and the Deathly Hallows', author: 'J.K Rowling', sales: 4475152 },
+  ];
+
+const book1 = books.filter((buku) => buku.sales > 1000000);
+console.log(book1)
+
+const greatAuthors = book1.map((buku) => { return `${buku.author} adalah penulis buku ${buku.title} yang sangat hebat!`});
+console.log(greatAuthors)
