07d612d02c7a9cc081c7958d6df3c24e353466af
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2022-12-27 23:49:52 +0700

    Add files via upload

---
+++ handling_errors.js
@@ -0,0 +1,31 @@
+// case 1
+let json = '{ "age": 20 }';
+ 
+try {
+    let user = JSON.parse(json);
+ 
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+ 
+    console.log(user.name); // undefined
+    console.log(user.age);  // 20
+} catch (error) {
+    console.log(`JSON Error: ${error.message}`);
+}
+
+// solve case 1
+let json2 = '{"name":"Agus", "age": 20 }';
+ 
+try {
+    let user = JSON.parse(json2);
+ 
+    if (!user.name) {
+        throw new SyntaxError("'name' is required.");
+    }
+ 
+    console.log(user.name); // undefined
+    console.log(user.age);  // 20
+} catch (error) {
+    console.log(`JSON Error: ${error.message}`);
+}
---
+++ try catch.js
@@ -0,0 +1,19 @@
+// try and catch
+try {
+    console.log("Awal blok try");   // (1)
+    errorCode;                      // (2)
+    console.log("Akhir blok try");  // (3)
+} catch (error) {
+    console.log("Terjadi error!");  // (4)
+}
+
+// try catch finally
+try {
+    console.log("Awal blok try");   // (1)
+    errorCode;                      // (2)
+    console.log("Akhir blok try");  // (3)
+} catch (error) {
+    console.log("Terjadi error!");  // (4)
+} finally{
+    console.log('Tetap dijalankan')
+}
