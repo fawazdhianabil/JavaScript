729209ee91c1b67dadb19b2c2a9b93b471d09d5b
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2022-12-27 16:00:43 +0700

    Add files via upload

---
+++ Date.js
@@ -0,0 +1,12 @@
+const date = new Date();
+ 
+const timeInJakarta = date.toLocaleString('id-ID', {
+  timeZone: 'Asia/Jakarta',
+});
+ 
+const timeInTokyo = date.toLocaleString('ja-JP', {
+  timeZone: 'Asia/Tokyo',
+});
+
+console.log(timeInJakarta)
+console.log(timeInTokyo)
